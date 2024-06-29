e9ff1aea9026652ebc002cb4518464526f02b669
pradeep_prasad <prasad.pradeep.pp@gmail.com>
2024-06-29 12:17:08 +0530

    first commit

---
+++ test_app/views.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
